a4099ccb4edc2a5aad6a4dbed5ec7d8b560f4860
Mahmoud Salah <ms685116@gmail.com>
2023-12-10 17:11:48 +0200

    configure new server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,27 +13,25 @@ chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
 printf %s "server {
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
-    root   /etc/nginx/html;
-    index  index.html index.htm;
     add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
 
     location /redirect_me {
-        return 301 https://github.com/mahmoudsalah296;
+	return 301 https://github.com/mahmoudsalah296;
     }
 
     error_page 404 /404.html;
     location /404 {
-        root /etc/nginx/html;
+        root /var/www/html;
         internal;
-    }
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
     }
 }" > /etc/nginx/sites-available/default
 
