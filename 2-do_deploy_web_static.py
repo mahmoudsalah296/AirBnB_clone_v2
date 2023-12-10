f173328490623b4ac4ef6f5b831ee261d92229c8
Mahmoud Salah <ms685116@gmail.com>
2023-12-10 23:24:42 +0200

    deploy content on servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -43,12 +43,6 @@ def do_deploy(archive_path):
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
