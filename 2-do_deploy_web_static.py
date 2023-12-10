3a484dc7a7cbc31c51f173343cd782a3c52eba49
Mahmoud Salah <ms685116@gmail.com>
2023-12-10 22:59:18 +0200

    deploy content on servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,27 +25,42 @@ def do_deploy(archive_path):
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
-    file_name = file.split(".")[0]
-    result = put(archive_path, "/tmp/{}").format(file)
-    if result.failed:
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    result = run("mkdir -p /data/web_static/releases/{}").format(file_name)
-    if result.failed:
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    result = run("tar xzf /tmp/{} -C /data/web_static/releases/{}").format(
-        file, file_name
-    )
-    if result.failed:
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    result = run("rm -rf /tmp/{}").format(file)
-    if result.failed:
+    if (
+        run(
+            "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)
+        ).failed
+        is True
+    ):
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if (
+        run(
+            "mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name)
+        ).failed
+        is True
+    ):
+        return False
+    if (
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed
+        is True
+    ):
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if (
-        run("ln -s /data/web_static/current /data/web_static/releases/{}")
-        .format(file_name)
-        .failed
+        run(
+            "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)
+        ).failed
         is True
     ):
         return False
