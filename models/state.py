5891568b5c7de495b892b554b7eb78684552b515
Eslam Mohamed <eslammhamed425@gmail.com>
2023-12-23 23:47:03 +0200

    fix errors

--- models/state.py
+++ models/state.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
+
+"""
+A module that defines the ORM class for State table
+"""
 import os
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """
+    Defines attributes for State table
+    """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete')
+    else:
+        name = ''
+
         @property
         def cities(self):
-            """ cities getter attribute """
-            cit_lis = []
-            all_cit = models.storage.all(City)
-            for city in all_cit.values():  # change .items() to values() as it
-                # returns an obj that contains values of a dictionary as a list
-                if city.state_id == self.id:
-                    cit_lis.append(city)
-            return cit_lis
+            """
+            Returns (list): List of City instances
+            with state_id equals to the current State.id
+            """
+            from models.city import City
+            from models import storage
+            city_list = list(storage.all(City).values())
+            return [city for city in city_list if city.state_id == self.id]
--- models/user.py
+++ models/user.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
+
+"""
+A module that defines the ORM class for User table
+"""
+from os import getenv
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """
+    Defines attributes for User table
+    """
     __tablename__ = 'users'
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=False)
-    last_name = Column('last_name', String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship(
+            'Place', backref='user', cascade='all, delete')
+        reviews = relationship(
+            'Review', backref='user', cascade='all, delete')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
