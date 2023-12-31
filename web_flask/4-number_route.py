296b762e6c4568509b63dc2a6a4f60f90e2aa36f
Mahmoud Salah <ms685116@gmail.com>
2023-12-31 13:40:31 +0200

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""hbnb routing"""
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """hello function"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb function"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def isfun(text):
+    """display text from url"""
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False, defaults={"text": "is cool"})
+def python_route(text):
+    """Display 'Python' followed by the value of the text variable)"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    """display 'n is a number' only if n is an integer"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
